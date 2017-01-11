17284476234ee161fe17998fc17085f061a1a200
Ashish Bende <eashishbende@gmail.com>
2017-01-11 10:29:25 -0800

    Adding adapter pattern

---
+++ Java/src/net/cnollege/designpatterns/java/structural/adapter/Source.java
@@ -0,0 +1,10 @@
+package net.cnollege.designpatterns.java.structural.adapter;
+
+/**
+ * Created by mugen on 1/11/17.
+ */
+public class Source {
+    public void printMessage(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ Java/src/net/cnollege/designpatterns/java/structural/adapter/Target.java
@@ -0,0 +1,8 @@
+package net.cnollege.designpatterns.java.structural.adapter;
+
+/**
+ * Created by mugen on 1/11/17.
+ */
+public interface Target {
+    public void sayHello();
+}
